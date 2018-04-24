13c1ad2c165c1834e0fac6e6dd356986150d11d4
Kevin Pena <kevinpena160@gmail.com>
2018-04-23 19:24:57 -0500

    working game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,56 @@
 $(document).ready(function () {
 
-
-    var randNum = (Math.floor(Math.random() * 120) + 19);
     var userNum = 0;
     var wins = 0;
     var losses = 0;
-    var buttonOne = (Math.floor(Math.random() * 12) + 1);
-    var buttonTwo = (Math.floor(Math.random() * 12) + 1);
-    var buttonThree = (Math.floor(Math.random() * 12) + 1);
-    var buttonFour = (Math.floor(Math.random() * 12) + 1);
-    
+    var running = true;
+    var randNum = 0;
+    var buttonOne = 0;
+    var buttonTwo = 0;
+    var buttonThree = 0;
+    var buttonFour = 0;
 
-    $("#ranNum").text(randNum);
+    function randomNumbers() {
+        randNum = (Math.floor(Math.random() * 120) + 19);
+        buttonOne = (Math.floor(Math.random() * 12) + 1);
+        buttonTwo = (Math.floor(Math.random() * 12) + 1);
+        buttonThree = (Math.floor(Math.random() * 12) + 1);
+        buttonFour = (Math.floor(Math.random() * 12) + 1);
+        userNum = 0;
+        $("#score").text(userNum);
+        $("#ranNum").text(randNum);
+    }
+
+    randomNumbers();
+    
     $("#wins").text(wins);
     $("#losses").text(losses);
+    
 
-    $("body").on("click", "#button1", function () {
+    $(".allButtons").on("click", "#button1", function () {
         userNum = userNum + buttonOne;
-        console.log(userNum);
     });
-    $("body").on("click", "#button2", function () {
+    $(".allButtons").on("click", "#button2", function () {
         userNum = userNum + buttonTwo;
-        console.log(userNum);
     });
-    $("body").on("click", "#button3", function () {
+    $(".allButtons").on("click", "#button3", function () {
         userNum = userNum + buttonThree;
-        console.log(userNum);
     });
-    $("body").on("click", "#button4", function () {
+    $(".allButtons").on("click", "#button4", function () {
         userNum = userNum + buttonFour;
-        console.log(userNum);
+    });
+    $(".allButtons").on("click", function () {
+        $("#score").text(userNum);
+        if (userNum === randNum) {
+            wins++;
+            randomNumbers();
+            $("#wins").text(wins);
+
+        } else if (userNum > randNum) {
+            losses++;
+            randomNumbers();
+            $("#losses").text(losses);
+        }
     });
 
-    if (userNum === randNum) {
-        wins++;
-        console.log(wins);
-    } else if (userNum > randNum) {
-        losses++;
-        console.log(losses)
-    }
 });
